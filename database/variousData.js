023361e21d86f951952ee48c406808ed62af99d7
davidvach <vachalek.david@gmail.com>
2021-02-28 15:29:00 +0100

    fixed array to lowercase

--- database/variousData.js
+++ database/variousData.js
@@ -24,11 +24,29 @@ var chapters = {
 }
 
 var dailyPrizes = {
-
+	
     "march_xxi_": [
-        "A_Evt_March_XXI_Phoenix_Chick",
+        /*"A_Evt_March_XXI_Phoenix_Chick",
 		"A_Evt_March_XXI_Eggshell_Pavillon",
-		"INS_RF_GRR_10"
+		"INS_RF_GRR_10",
+		"A_Evt_March_XXI_Eggshell_Shrine",
+		"INS_UNIT_LM_15",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",*/
     ],
 
     "february_xxi_": [
