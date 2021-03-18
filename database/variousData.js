1f3c4a1ad0bd992cf5683ce1255a3722f7bfbc41
davidvach <61384041+davidvach@users.noreply.github.com>
2021-03-18 08:47:51 +0100

    added dp

--- database/variousData.js
+++ database/variousData.js
@@ -42,14 +42,15 @@ var dailyPrizes = {
         "A_Evt_Easter_Vii_BurningEgg",
         "INS_UNIT_MC_LR_10",
         "A_Evt_Golden_Palace",
-        "INS_RF_GRR_10", //"A_Evt_March_XXI_Phoenix_Chick",
+        "A_Evt_March_XXI_Phoenix_Chick", //"",
         "A_Evt_March_XXI_Eggshell_Shrine",
         "INS_UNIT_LM_15",
         "A_Evt_Easter_XX_Phoenix_Anatomy",
         "INS_KP_AW_10",
         "A_Evt_March_XXI_Temple_of_Embers",
         "A_Evt_March_XXI_Phoenix_Water_Mill",
-        "A_Evt_March_XXI_Phoenix_Rider"
+        "A_Evt_March_XXI_Phoenix_Rider",
+        "A_Evt_March_XXI_Eggshell_Shrine" //?
     ],
 
     "february_xxi_": [
