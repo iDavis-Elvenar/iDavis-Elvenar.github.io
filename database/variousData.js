3d140e266fd6e1518174c5cfa40e35be63e01ee0
davidvach <vachalek.david@gmail.com>
2021-04-08 17:51:51 +0200

    ch18 released

--- database/variousData.js
+++ database/variousData.js
@@ -18,7 +18,7 @@ var prioritiesProduction = ["money", "supplies", "marble", "steel", "planks", "c
     "", "", "", "", "", "", "", "", "", "", "", "",
     "", "", "", "", "", "", "", "", "", "", "" ];
 
-var numberOfChapters = 17;
+var numberOfChapters = 18;
 
 var chapters = {
     1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X',
