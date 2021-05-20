2f6b8eb744292afa6e8fbd5ca6065f6fdc25a374
davidvach <61384041+davidvach@users.noreply.github.com>
2021-05-20 13:09:00 +0200

    june_xxi video

--- database/variousData.js
+++ database/variousData.js
@@ -39,7 +39,8 @@ let eventBanners = {
 let eventVideos = {
     "february_xxi_": "https://www.youtube.com/embed/AS2XrTrF8PM",
     "march_xxi_": "https://www.youtube.com/embed/6VoSOv1ZXFA",
-    "may_xxi_": "https://www.youtube.com/embed/lQ-iFRKuFvU"
+    "may_xxi_": "https://www.youtube.com/embed/lQ-iFRKuFvU",
+    "june_xxi_": "https://www.youtube.com/embed/doNM1AbVWt8"
 }
 
 let eventNames = {
