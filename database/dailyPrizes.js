af148eb0634514774203dac23e554e883bb8c52c
davidvach <61384041+davidvach@users.noreply.github.com>
2021-06-04 15:02:14 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -18,7 +18,8 @@ var dailyPrizes = {
         "A_Evt_June_XXI_Unexpected_Morning",
         "INS_KP_AW_10",
         "A_Evt_June_XXI_Active_Fissure",
-        "A_Evt_July_XX_Copper_Dragon", //--<
+        "A_Evt_July_XX_Copper_Dragon",
+        "INS_UNIT_TG_HR_10", //--<
         "INS_RF_GRR_10",
         "A_Evt_June_XXI_Body_Transmutation",
         "A_Evt_July_XX_Hedge_Beard_Treant",
