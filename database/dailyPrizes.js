b79f6e20cd9e2b0da9727d536006ec20db199ef8
davidvach <61384041+davidvach@users.noreply.github.com>
2021-06-29 08:20:45 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -8,7 +8,8 @@ var dailyPrizes = {
         "A_Evt_July_XXI_Manta_Boat",
         "A_Evt_MM_XIX_Corals",
         "INS_KP_AW_10",
-        "A_Evt_MM_XIX_CrabHotel"
+        "A_Evt_MM_XIX_CrabHotel",
+        "A_Evt_MM_XIX_SharkTower"
     ],
 
     "june_xxi_": [
