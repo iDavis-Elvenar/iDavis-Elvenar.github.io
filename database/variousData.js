32ffac733a41c7ea9463142f3b4a21191033b68d
davidvach <61384041+davidvach@users.noreply.github.com>
2021-07-12 13:02:53 +0200

    july_xxi video

--- database/variousData.js
+++ database/variousData.js
@@ -41,7 +41,8 @@ let eventVideos = {
     "february_xxi_": "https://www.youtube.com/embed/AS2XrTrF8PM",
     "march_xxi_": "https://www.youtube.com/embed/6VoSOv1ZXFA",
     "may_xxi_": "https://www.youtube.com/embed/lQ-iFRKuFvU",
-    "june_xxi_": "https://www.youtube.com/embed/doNM1AbVWt8"
+    "june_xxi_": "https://www.youtube.com/embed/doNM1AbVWt8",
+    "july_xxi_": "https://www.youtube.com/embed/sWa4cT4yGIA"
 }
 
 let eventNames = {
