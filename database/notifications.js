01cad75598e2a757dbf78d3c9c4c487804d1c7ed
davidvach <vachalek.david@gmail.com>
2021-08-06 17:09:42 +0200

    notifications feature

---
+++ database/notifications.js
@@ -0,0 +1,20 @@
+var notifications = {
+    "1":{
+        "id":1,
+        "text":"Do you have any feedback for me or did you find a bug at the website? Feel free to reach me at new <a href='contact.html'><strong>Contact Page</strong></a> in the top left menu.",
+        "eachSeconds":604800,
+        "style":"warning",
+        "duration":10,
+        "active":true,
+        "priority":20
+    },
+    "2":{
+        "id":2,
+        "text":"There are new features on the website! Check the list of everything new on the updated <a href='index.html'><strong>Home Page</strong></a>.",
+        "eachSeconds":1000000,
+        "style":"warning",
+        "duration":10,
+        "active":false,
+        "priority":10
+    },
+}
---
+++ javascript/notificationHandler.js
@@ -0,0 +1,65 @@
+function showNotification(id) {
+    //console.log("OPENED NOTIFICATION WITH ID "+id.toString())
+    markOpenedNotification(id);
+    create_exception(notifications[id]["text"], notifications[id]["duration"], notifications[id]["style"]);
+}
+
+window.addEventListener('load',
+    function() {
+        removeOldNotificationsFromLocalStorage();
+        prepareSpaceForNewNotifications();
+        let id = findNotificationToOpen();
+        ((id > 0) ? setTimeout(function() { showNotification(id); }, 5000) : {});
+    }, false);
+
+function numberOfNotifications() {
+    return Object.keys(notifications).length;
+}
+
+function removeOldNotificationsFromLocalStorage() {
+    for (var variable in localStorage) {
+        if (variable.startsWith("idavis-notification_") &&
+        parseInt(variable.split("_")[1]) > numberOfNotifications()) {
+            localStorage.removeItem(variable);
+        }
+    }
+}
+
+function prepareSpaceForNewNotifications() {
+    for (let i = 1; i <= numberOfNotifications(); i++) {
+        if (localStorage.getItem("idavis-notification_"+i.toString()) === null) {
+            markOpenedNotification(i, false);
+        }
+    }
+}
+
+function markOpenedNotification(id, showed=true) {
+    if (showed) {
+        localStorage.setItem("idavis-notification_"+id.toString(), Date.now().toString());
+    } else {
+        localStorage.setItem("idavis-notification_"+id.toString(), "0");
+    }
+}
+
+function findNotificationToOpen() {
+    let activeNotifications = [];
+    for (let i = 1; i <= numberOfNotifications(); i++) {
+        if (isAvailable(i)) {
+            activeNotifications.push(notifications[i]);
+        }
+    }
+    activeNotifications.sort((a,b) => (a.priority > b.priority) ? -1 : 1);
+    //console.log("AVAILABLE: "+activeNotifications.length)
+    return ((activeNotifications.length > 0) ? activeNotifications[0].id : -1);
+}
+
+function isAvailable(id) {
+    if (notifications[id]["active"] === false) {
+        return false;
+    }
+    if ((((Date.now() - parseInt(localStorage.getItem("idavis-notification_"+id.toString()))))/1000)
+    < notifications[id].eachSeconds) {
+        return false;
+    }
+    return true;
+}
