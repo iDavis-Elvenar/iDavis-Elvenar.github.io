12f8991be5484577d215ce888b0c646dd901ccc2
davidvach <61384041+davidvach@users.noreply.github.com>
2021-09-30 11:44:33 +0200

    Update variousData.js

--- database/variousData.js
+++ database/variousData.js
@@ -45,7 +45,8 @@ let eventVideos = {
     "may_xxi_": "https://www.youtube.com/embed/lQ-iFRKuFvU",
     "june_xxi_": "https://www.youtube.com/embed/doNM1AbVWt8",
     "july_xxi_": "https://www.youtube.com/embed/sWa4cT4yGIA",
-    "september_xxi_": "https://www.youtube.com/embed/DTGy8Q0lnAc"
+    "september_xxi_": "https://www.youtube.com/embed/DTGy8Q0lnAc",
+    "october_xxi_": "https://www.youtube.com/embed/PzUHa4Bat2k"
 }
 
 let eventNames = {
