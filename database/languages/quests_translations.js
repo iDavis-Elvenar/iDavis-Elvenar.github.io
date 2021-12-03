77962fa66f1f1e101deb020eb9950fe8ed7346ac
davidvach <61384041+davidvach@users.noreply.github.com>
2021-12-03 21:28:48 +0100

    Update quests_translations.js

--- database/languages/quests_translations.js
+++ database/languages/quests_translations.js
@@ -2,6 +2,6 @@ function questTranslate(text) {
     if (localStorage.getItem("lang") === "cz") {
         return text;
     } else {
-        return "EN Required";
+        return "Only CZ language supported for now, more languages are coming soon! / Zvoľte si český jazyk z glóbusu v hornej lište pre zobrazenie úloh.";
     }
 }
