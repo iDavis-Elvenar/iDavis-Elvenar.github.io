3b6bca4aec648be782515f50e8f2b97b37ee509a
davidvach <vachalek.david@gmail.com>
2021-12-11 17:57:02 +0100

    Quests translation system

--- database/languages/quests_translations.js
+++
@@ -1,7 +0,0 @@
-function questTranslate(text) {
-    if (localStorage.getItem("lang") === "cz") {
-        return text;
-    } else {
-        return "Only CZ language supported for now, more languages are coming soon! / Zvoľte si český jazyk z glóbusu v hornej lište pre zobrazenie úloh.";
-    }
-}
