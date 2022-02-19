1d6f1238f65fd3f5369f94738cbf3731e87a1d70
davidvach <61384041+davidvach@users.noreply.github.com>
2022-02-19 11:35:57 +0100

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -21,6 +21,7 @@ var dailyPrizes = {
         "A_Evt_Passageway",
         "INS_UNIT_TG_MA_10",
         "A_Evt_February_XXII_Furry_Sands",
+        "A_Evt_June_XX_Dwarf_Ship",
     ],
     
     "january_xxii_": [
