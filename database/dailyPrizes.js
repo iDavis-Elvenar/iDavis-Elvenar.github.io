7d253d597a63adc26fe66a51034a3d5c8ad31918
davidvach <61384041+davidvach@users.noreply.github.com>
2022-04-11 05:14:21 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -5,6 +5,7 @@ var dailyPrizes = {
         "A_Evt_May_XXI_Bumblebee_Stables",
         "goods_standard_sentient_{1}",  //A pack of standard or sentient goods (depending on chapter)
         "A_Evt_May_XX_Food_Market",
+        "INS_UNIT_MA_10",
     ],
     
     "easter_xxii_": [
