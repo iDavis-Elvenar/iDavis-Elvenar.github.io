343a61ad57ccba8423c513e9159dcf2d629e0772
davidvach <61384041+davidvach@users.noreply.github.com>
2022-04-18 10:21:59 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -16,6 +16,7 @@ var dailyPrizes = {
         "runeshards-1_{4}",
         "A_Evt_May_XXII_Tender_Tag_Team",
         "INS_KP_AW_20",
+        "INS_RF_GRR_5{2}",
     ],
     
     "easter_xxii_": [
