af71cab373fa2c81986a00590d2ba23a8e83f15f
davidvach <61384041+davidvach@users.noreply.github.com>
2022-04-21 10:56:30 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -19,6 +19,7 @@ var dailyPrizes = {
         "INS_RF_GRR_5{2}",
         "A_Evt_May_XX_Cotton_Candy_Magic",
         "INS_UNIT_TG_HR_10",
+        "A_Evt_May_XXII_Tender_Tag_Team",
     ],
     
     "easter_xxii_": [
