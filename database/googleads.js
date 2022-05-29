dc20a11a267a1d0025eff654f9e2acac21e449a0
davidvach <vachalek.david@gmail.com>
2022-05-29 19:23:18 +0200

    ga size adjustment

--- database/googleads.js
+++ database/googleads.js
@@ -1,13 +1,14 @@
 let googleads = {
-    "content_neutral": `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4154227292627045"
-                            crossorigin="anonymous"></script>
-                        <ins class="adsbygoogle"
-                            style="display:block"
-                            data-ad-format="fluid"
-                            data-ad-layout-key="-6t+ed+2i-1n-4w"
-                            data-ad-client="ca-pub-4154227292627045"
-                            data-ad-slot="7940520800"></ins>
-                        <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>`,
+    "horizontal": `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4154227292627045"
+    crossorigin="anonymous"></script> <!-- In-article -->
+<ins class="adsbygoogle"
+    style="display:block; text-align:center;"
+    data-ad-layout="in-article"
+    data-ad-format="fluid"
+    data-ad-client="ca-pub-4154227292627045"
+    data-ad-slot="9774392946"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>`,
+    "two_blocks": ``,
 }
