1de3920bb92af56b5621be69470580d9a50ca8b4
davidvach <61384041+davidvach@users.noreply.github.com>
2022-10-19 09:37:54 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -21,6 +21,7 @@ var dailyPrizes = {
         "INS_KP_AW_20",
         "A_Evt_October_XXII_Creepy_Clinic",
         "INS_REV_SQD_50",
+        "A_Evt_October_XX_Gargoyle_Fountain",
     ],
     
     "autumn_xxii_": [
