766962ee2067aadada9505770de63d7e9e44cf6d
davidvach <61384041+davidvach@users.noreply.github.com>
2023-01-14 12:07:46 +0100

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -22,6 +22,7 @@ var dailyPrizes = {
         "A_Evt_June_XXI_Body_Transmutation",
         "INS_UNIT_TG_MA_10",
         "A_Evt_July_XX_Astral_Crevice",
+        "A_Evt_January_XXIII_Experimental_Distillery",
     ],
     
     "december_xxii_": [
