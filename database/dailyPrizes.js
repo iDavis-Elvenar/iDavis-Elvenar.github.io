11f69e563ace17e30f6112f2ecdc9ed38e2e1827
davidvach <vachalek.david@gmail.com>
2023-03-15 13:29:04 +0100

    easter_xxiii relics

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -3,6 +3,7 @@ var dailyPrizes = {
     "easter_xxiii_": [
         "A_Evt_Easter_XX_Phoenix_Lava",
         "A_Evt_March_XXI_Phoenix_Chick",
+        "INS_UNIT_MC_HM_10",
     ],
 
     "february_xxiii_": [
