70666f7ca7103df473b86e83db5c35c3df132cde
davidvach <vachalek.david@gmail.com>
2023-06-20 14:48:02 +0200

    all events dp selected link fix

--- javascript/all_events_v3.js
+++ javascript/all_events_v3.js
@@ -11,17 +11,22 @@ function changeEventBanner() {
 
 function loadPage() {
     if (location.href.split('#').length > 1 && location.href.split('#')[1] !== "") {
-        if (location.href.split('#')[1].split('-')[1] !== "") {
-            eventId = location.href.split('#')[1].split('-')[1];
-            eventSelect = document.getElementById("input_event");
-            for (let option = 0; option < eventSelect.options.length; option++) {
-                if (eventSelect.options[option].value === location.href.split('#')[1].split('-')[1]) {
-                    eventSelect.selectedIndex = option;
+        if (location.href.split('#')[1].includes("day")) {
+            location.href = location.href.split('#')[0];
+            displayDailyPrizes();
+        } else {
+            if (location.href.split('#')[1].split('-')[1] !== "") {
+                eventId = location.href.split('#')[1].split('-')[1];
+                eventSelect = document.getElementById("input_event");
+                for (let option = 0; option < eventSelect.options.length; option++) {
+                    if (eventSelect.options[option].value === location.href.split('#')[1].split('-')[1]) {
+                        eventSelect.selectedIndex = option;
+                    }
                 }
             }
+            foundView = location.href.split('#')[1].split("-")[0];
+            switchView(foundView);
         }
-        foundView = location.href.split('#')[1].split("-")[0];
-        switchView(foundView);
     } else {
         displayDailyPrizes();
     }
