b0a85031d71cb02e48edfdc1d9c31f3b8f212783
davidvach <61384041+davidvach@users.noreply.github.com>
2023-08-21 11:01:54 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -19,6 +19,7 @@ var dailyPrizes = {
         "frog_runeshards2_{2}",
         "A_Evt_Prod_Nutcracker",
         "INS_KP_AW_20",
+        "A_Evt_September_XX_Kite_Outpost",
     ],
 
     "scroll_aquatic_xxiii_": [
