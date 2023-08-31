e5ad613348314f8f78c9832608a52767b9722a96
davidvach <61384041+davidvach@users.noreply.github.com>
2023-08-31 15:45:11 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -2,6 +2,7 @@ var dailyPrizes = {
 
     "merge_dwarvengame_xxiii_": [
         "A_Evt_Merge_Dwarvengame_XXIII_Perpetual_Slider",
+        "A_Evt_August_XXII_Lunar_Factory",
     ],
 
     "theater_zodiac_xxiii_": [
