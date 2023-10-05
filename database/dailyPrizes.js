9f54884e17731964e17ad8d1dd9913e2dabc2ee6
davidvach <61384041+davidvach@users.noreply.github.com>
2023-10-05 11:37:52 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -4,6 +4,7 @@ var dailyPrizes = {
         "A_Evt_October_XXII_Creepy_Clinic",
         "A_Evt_Tile_Mistyforest_XXIII_Wraith_Unicorn",
         "INS_KP_{15}",
+        "A_Evt_October_XX_Scarriage",
     ],
 
     "merge_dwarvengame_xxiii_": [
