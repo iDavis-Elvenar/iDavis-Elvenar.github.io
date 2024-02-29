d87a50cfcdaa4f8bfd5eb0bec8ca6f21efa75d9a
davidvach <61384041+davidvach@users.noreply.github.com>
2024-02-29 10:03:57 +0100

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -24,6 +24,7 @@ var dailyPrizes = {
         "A_Evt_Easter_XXII_Rod_of_Ascleptius",
         "A_Evt_Easter_Vii_FireTower",
         "INS_UNIT_MC_HM_10",
+        "A_Evt_Easter_XXII_Shamanic_Healing_Grounds",
     ],
 
     "scroll_sorcerers_xxiv_": [
