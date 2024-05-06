0388019d0657a7c03ff174deaa7e1727df3207c3
davidvach <61384041+davidvach@users.noreply.github.com>
2024-05-06 11:36:32 +0200

    Update eventsSelectors.js

--- database/eventsSelectors.js
+++ database/eventsSelectors.js
@@ -1,7 +1,7 @@
 var allEvents = {
     "all_buildings" : { //NAME,ID,SELECTED,DISABLED == HIDDEN
         "2024" : [
-            ["Whiskers of the Past","tile_amuni_xxiv_",false,false],
+            ["Whiskers of the Past","tile_amuni_xxiv_",false,true],
             ["Buzzing Spring","shuffle_garden_xxiv_",true,false],
             ["Blessing of the Phoenix","theater_easter_xxiv",false,false],
             ["Tournament of Magic","scroll_sorcerers_xxiv_",false,false],
@@ -60,7 +60,7 @@ var allEvents = {
     },
     "all_events" : {
         "2024" : [
-            ["Whiskers of the Past","tile_amuni_xxiv_",false,false],
+            ["Whiskers of the Past","tile_amuni_xxiv_",false,true],
             ["Buzzing Spring","shuffle_garden_xxiv_",true,false],
             ["Blessing of the Phoenix","theater_easter_xxiv_",false,false],
             ["Tournament of Magic","scroll_sorcerers_xxiv_",false,false],
