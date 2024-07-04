79c9fd3756235fefe4779a2620471148847a52d5
davidvach <61384041+davidvach@users.noreply.github.com>
2024-07-04 09:31:19 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -3,6 +3,7 @@ var dailyPrizes = {
     "merge_dwarvengame_xxiv_": [
         "A_Evt_August_XXII_Lunar_Factory",
         "A_Evt_Merge_Dwarvengame_XXIII_Target_Shooting",
+        "INS_REV_SQD_50",
     ],
 
     "scroll_aquatic_xxiv_": [
