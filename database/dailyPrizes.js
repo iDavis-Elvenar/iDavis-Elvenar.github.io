e6f9904f85bb43cc2e5ad433ec21a75f6bf62f3f
davidvach <61384041+davidvach@users.noreply.github.com>
2024-07-17 14:14:50 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -16,6 +16,7 @@ var dailyPrizes = {
         "A_Evt_Car_XX_Dwarven_Wagon",
         "frog_grr_5_{1}",
         "A_Evt_August_XXII_Solar_Training_Grounds",
+        "A_Evt_StoneHengeSmall",
     ],
 
     "scroll_aquatic_xxiv_": [
