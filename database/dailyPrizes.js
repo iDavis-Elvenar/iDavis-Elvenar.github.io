7e3987cffa63bcf516f1b3fd3f5631a328c64cf3
davidvach <61384041+davidvach@users.noreply.github.com>
2024-08-27 09:07:05 +0200

    Update dailyPrizes.js

--- database/dailyPrizes.js
+++ database/dailyPrizes.js
@@ -17,6 +17,7 @@ var dailyPrizes = {
         "frog_runeshards2_{3}",
         "A_Evt_September_XX_Bell_Fountain",
         "INS_RF_CN_50",
+        "INS_UNIT_MC_LM_10",
     ],
 
     "merge_dwarvengame_xxiv_": [
