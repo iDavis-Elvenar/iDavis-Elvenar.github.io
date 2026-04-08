c2fd47da37b7288cc2761713fe71f00867451154
davidvach <vachalek.david@gmail.com>
2026-04-08 11:13:39 +0200

    Update eventsSelectors.js

--- database/eventsSelectors.js
+++ database/eventsSelectors.js
@@ -1,8 +1,8 @@
 var allEvents = {
     "all_buildings" : { //NAME,ID,SELECTED,DISABLED == HIDDEN
         "2026" : [
-            ["The Living Chorus","shuffle_garden_xxvi_",false,false],
-            ["Cosmic Phoenix Rite","theater_easter_xxvi_",true,false],
+            ["The Living Chorus","shuffle_garden_xxvi_",true,false],
+            ["Cosmic Phoenix Rite","theater_easter_xxvi_",false,false],
             ["The Summit Trials","merge_dwarvengame_xxvi_",false,false],
             ["The Duplication Disaster","scroll_sorcerers_xxvi_",false,false],
         ],
@@ -83,8 +83,8 @@ var allEvents = {
     },
     "all_events" : {
         "2026" : [
-            ["The Living Chorus","shuffle_garden_xxvi_",false,false],
-            ["Cosmic Phoenix Rite","theater_easter_xxvi_",true,false],
+            ["The Living Chorus","shuffle_garden_xxvi_",true,false],
+            ["Cosmic Phoenix Rite","theater_easter_xxvi_",false,false],
             ["The Summit Trials","merge_dwarvengame_xxvi_",false,false],
             ["The Duplication Disaster","scroll_sorcerers_xxvi_",false,false],
         ],
